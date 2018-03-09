e55cf3a37e3e0bae400a04cda9cf89fdf627d733
Benjamin Welker <bwelker91@gmail.com>
2018-03-09 18:09:19 -0500

    Added login with token.

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,36 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
         private readonly DataContext _context;
         public ValuesController(DataContext context)
         {
-            this._context = context;
-
+            _context = context;
         }
+        
         // GET api/values
         [HttpGet]
-        public async Task<IActionResult> GetValues()//IEnumerable<string> Get()
+        public async Task<IActionResult> GetValues()
         {
             var values = await _context.Values.ToListAsync();
+
             return Ok(values);
-            //return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetValue(int id)//string Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
             return Ok(value);
-            //return "value";
         }
 
         // POST api/values
--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -10,35 +10,33 @@ namespace DatingApp.API.Data
         private readonly DataContext _context;
         public AuthRepository(DataContext context)
         {
-            this._context = context;
-
+            _context = context;
         }
         public async Task<User> Login(string username, string password)
         {
             var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
 
-            if(user == null) {
+            if (user == null)
                 return null;
-            }
 
-            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) {
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
                 return null;
-            }
-
+            
+            // auth successful
             return user;
         }
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)) {
-                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                for(int i = 0; i < computedHash.Length; i++) {
-                    if(computedHash[i] != passwordHash[i]) {
-                        return false;
-                    }
-                }
-            }
-            return true;
+           using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+           {
+               var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+               for (int i = 0; i < computedHash.Length; i++)
+               {
+                   if (computedHash[i] != passwordHash[i]) return false;
+               }
+           }
+           return true;
         }
 
         public async Task<User> Register(User user, string password)
@@ -49,7 +47,7 @@ namespace DatingApp.API.Data
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
 
-            await _context.AddAsync(user);
+            await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
 
             return user;
@@ -57,18 +55,18 @@ namespace DatingApp.API.Data
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512()) {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
+           using (var hmac = new System.Security.Cryptography.HMACSHA512())
+           {
+               passwordSalt = hmac.Key;
+               passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+           }
         }
 
         public async Task<bool> UserExists(string username)
         {
-            if(await _context.Users.AnyAsync(x => x.Username == username)) {
+            if (await _context.Users.AnyAsync(x => x.Username == username))
                 return true;
-            }
-
+            
             return false;
         }
     }
